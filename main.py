2c92eed846575102c9ccba66d5790233c8c13912
jaceygan <jaceygan@gmail.com>
2023-11-20 23:40:47 +0800

    added search function

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 from bs4 import BeautifulSoup
 import requests
 
+searchString = 'm2 air 13 starlight'
+searchList = searchString.upper().split()
+
+
 mainurl = "https://www.apple.com/sg/shop/refurbished/mac/macbook-air"
 urlprefix = "https://www.apple.com"
 response = requests.get(mainurl).text
 
 soup = BeautifulSoup(response, 'lxml')
-products = soup.find_all('h3')
+products = soup.find_all('h3') #this returns some other garbage that is taken care of later
+
+productDict = {}
+
+def searchMatch(sl, p):
+    pUp = p.upper()
+    for s in sl:
+        if s.upper() not in pUp:
+            return False
+    return True
 
 for product in products:
     if product.find('a'):
-        print(product.text.replace('Refurbished', '').strip()+':')
-        print(urlprefix+product.find('a')['href'])
+        productDict[product.text.replace('Refurbished', '').strip()] = urlprefix+product.find('a')['href']
+        #print(product.text.replace('Refurbished', '').strip()+':')
+        #print(urlprefix+product.find('a')['href'])
+counter = 0
+for p in productDict.keys():
+    if searchMatch(searchList, p):
+        counter +=1
+        print(p)
+        print(productDict[p])
+print (counter, 'items found')
+
 
 
