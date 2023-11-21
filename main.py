0908c2a601942ec708f74e85ffe8d9f437f3c20b
jaceygan <jaceygan@gmail.com>
2023-11-21 10:24:38 +0800

    mostly done. left with email

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-searchString = 'm2 air 13 starlight'
-searchList = searchString.upper().split()
+searchList= 'air 15 starlight m2'.split()
 
+sendEmail = True
 
-mainurl = "https://www.apple.com/sg/shop/refurbished/mac/macbook-air"
+mainurl = "https://www.apple.com/sg/shop/refurbished/mac"
 urlprefix = "https://www.apple.com"
 response = requests.get(mainurl).text
 
 soup = BeautifulSoup(response, 'lxml')
 products = soup.find_all('h3') #this returns some other garbage that is taken care of later
-
-productDict = {}
 
 def searchMatch(sl, p):
     pUp = p.upper()
@@ -21,18 +19,30 @@ def searchMatch(sl, p):
             return False
     return True
 
+count = 0
 for product in products:
-    if product.find('a'):
-        productDict[product.text.replace('Refurbished', '').strip()] = urlprefix+product.find('a')['href']
-        #print(product.text.replace('Refurbished', '').strip()+':')
-        #print(urlprefix+product.find('a')['href'])
-counter = 0
-for p in productDict.keys():
-    if searchMatch(searchList, p):
-        counter +=1
-        print(p)
-        print(productDict[p])
-print (counter, 'items found')
+    if product.find('a'): #conveiently, the other garbage doesnt have a href
+        pName = product.text.replace('Refurbished', '').strip()
+        if searchMatch(searchList, pName):
+            count += 1
+            pURL = urlprefix+product.find('a')['href']  
+            pDetails = requests.get(pURL).content
+            pSoup = BeautifulSoup(pDetails, 'lxml')
+            print(str(count)+ '. '+ pName)
+            print()
+
+            print("Price:",pSoup.find("div", class_="rf-pdp-currentprice").text)
+            print('Info:')
+            
+            for info in pSoup.find("div", class_="rc-pdsection-mainpanel column large-9 small-12"):
+                print(info.text.strip())
+
+            print(pURL)
+            print()
+            
+            print ("=================================================")
+            print()
 
 
-
+print(count, "items found")
+            
