d9940bebaae1daf1d0921de4465fa4240a3f92f8
jaceygan <69943651+jaceygan@users.noreply.github.com>
2023-11-21 11:57:01 +0800

    Delete emailproperties.py

--- emailproperties.py
+++
@@ -1,9 +0,0 @@
-
-
-sender = 'pracdevemail@gmail.com'
-reciepient= 'jaceygan@gmail.com'
-subject = 'Updates from applerefurbscrapper'
-
-f= open('emailp.txt', 'r')
-password=f.read()
-#print(password)
